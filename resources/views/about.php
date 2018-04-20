8014e9a883ac123572732b2e994f96c8ce8e0fe4
aldosoni1 <aldosoni1@gmail.com>
2018-04-19 23:57:15 -0500

    Prueba

--- resources/views/about.php
+++ resources/views/about.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Acerca de nosotros!
+                    HOLI!
                 </div>
 
                 <div class="links">
